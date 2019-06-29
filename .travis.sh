0ffb6bddd6ecc6772a23f29d86efc4580ec7799a
Mark Raynsford <code@io7m.com>
2019-06-29 14:55:46 +0000

    Make and publish signed release builds in Travis

--- .travis.sh
+++ .travis.sh
@@ -6,6 +6,9 @@ then
   exit 1
 fi
 
+#------------------------------------------------------------------------
+# Configure SSH
+
 mkdir -p "${HOME}/.ssh" || exit 1
 echo "${LFA_BUILDS_SSH_KEY}" | base64 -d > "${HOME}/.ssh/id_ed25519" || exit 1
 chmod 700 "${HOME}/.ssh" || exit 1
@@ -16,6 +19,11 @@ chmod 600 "${HOME}/.ssh/id_ed25519" || exit 1
 EOF
 ) >> "$HOME/.ssh/known_hosts" || exit 1
 
+#------------------------------------------------------------------------
+# Configure Nexus and keystore
+
+scp -P 1022 travis-ci@builds.lfa.one:lfa-keystore.jks .
+
 (cat <<EOF
 
 nexusUsername = notausername
@@ -23,6 +31,12 @@ nexusPassword = notapassword
 EOF
 ) >> gradle.properties || exit 1
 
+#------------------------------------------------------------------------
+# Build!
+
 ./gradlew clean assemble test || exit 1
 
-scp -P 1022 -v -v ./au.org.libraryforall.updater.app/build/outputs/apk/debug/*.apk travis-ci@builds.lfa.one:/sites/builds.lfa.one/apk/ || exit 1
+#------------------------------------------------------------------------
+# Publish APKs
+
+scp -P 1022 -v -v ./au.org.libraryforall.updater.app/build/outputs/apk/release/*.apk travis-ci@builds.lfa.one:/sites/builds.lfa.one/apk/ || exit 1
