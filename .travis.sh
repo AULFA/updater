1e2512812930b21dec4d660caba5462c64e5f9b3
Mark Raynsford <code@io7m.com>
2019-06-29 09:53:20 +0000

    Append fake username/password

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 (cat <<EOF
+
 nexusUsername = notausername
 nexusPassword = notapassword
 EOF
-) > gradle.properties.tmp || exit 1
-
-mv gradle.properties.tmp gradle.properties || exit 1
+) >> gradle.properties || exit 1
 
 exec ./gradlew clean assemble test
