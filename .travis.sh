24a98d0700614e8a3210b005db86d1544fb2d45f
Mark Raynsford <code@io7m.com>
2019-06-29 09:42:51 +0000

    Remove LFA_* variable requirements

--- .travis.sh
+++ .travis.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-if [ -z "${LFA_BUILDS_USER}" ]
-then
-  echo "error: LFA_BUILDS_USER is not defined" 1>&2
-  exit 1
-fi
-
-if [ -z "${LFA_BUILDS_PASSWORD}" ]
-then
-  echo "error: LFA_BUILDS_PASSWORD is not defined" 1>&2
-  exit 1
-fi
-
 (cat <<EOF
 
 EOF
