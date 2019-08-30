0de13e87297f5ee0d521706f0e96d81fea62f4c6
Mark Raynsford <code@io7m.com>
2019-08-30 17:16:40 +0000

    Log all messages to a file

--- au.org.libraryforall.updater.app/src/main/java/au/org/libraryforall/updater/app/MainApplication.kt
+++ au.org.libraryforall.updater.app/src/main/java/au/org/libraryforall/updater/app/MainApplication.kt
@@ -1,14 +1,43 @@
 package au.org.libraryforall.updater.app
 
 import android.app.Application
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.core.rolling.RollingFileAppender
 import org.slf4j.LoggerFactory
+import java.io.File
 
 class MainApplication : Application() {
 
   private val logger = LoggerFactory.getLogger(MainApplication::class.java)
 
+  /**
+   * We apparently can't rely on the paths configured in logback.xml to actually work
+   * correctly across different devices. This bit of code tries to configure the path
+   * of the log file directly.
+   */
+
+  private fun configureLogging() {
+    try {
+      val context = LoggerFactory.getILoggerFactory() as LoggerContext
+      for (logger in context.getLoggerList()) {
+        val index = logger.iteratorForAppenders()
+        while (index.hasNext()) {
+          val appender = index.next()
+          if (appender is RollingFileAppender<*>) {
+            (appender as RollingFileAppender<*>).file = File(externalCacheDir, "log.txt").absolutePath
+            appender.start()
+          }
+        }
+      }
+      this.logger.debug("logging is configured")
+    } catch (e: Exception) {
+      this.logger.error("could not configure logging: ", e)
+    }
+  }
+
   override fun onCreate() {
     super.onCreate()
+    this.configureLogging()
     this.logger.debug("starting")
     MainServices.initialize(this)
   }
