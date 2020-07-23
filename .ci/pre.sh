ac94abf891db0680491db9515631c8c09be19925
Mark Raynsford <code@io7m.com>
2020-07-23 14:16:07 +0000

    Update CI scripts

---
+++ .ci/pre.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+exec &> >(tee -a ".ci/pre.log")
+
+#------------------------------------------------------------------------
+# Utility methods
+#
+
+fatal()
+{
+  echo "fatal: $1" 1>&2
+  echo
+  echo "dumping log: " 1>&2
+  echo
+  cat .ci/pre.log
+  exit 1
+}
+
+info()
+{
+  echo "info: $1" 1>&2
+}
+
+#------------------------------------------------------------------------
+# Check environment
+#
+
+if [ -z "${MAVEN_CENTRAL_USERNAME}" ]
+then
+  fatal "MAVEN_CENTRAL_USERNAME is not defined"
+fi
+if [ -z "${MAVEN_CENTRAL_PASSWORD}" ]
+then
+  fatal "MAVEN_CENTRAL_PASSWORD is not defined"
+fi
+if [ -z "${MAVEN_CENTRAL_STAGING_PROFILE_ID}" ]
+then
+  fatal "MAVEN_CENTRAL_STAGING_PROFILE_ID is not defined"
+fi
+if [ -z "${MAVEN_CENTRAL_SIGNING_KEY_ID}" ]
+then
+  fatal "MAVEN_CENTRAL_SIGNING_KEY_ID is not defined"
+fi
+if [ -z "${LFA_GITHUB_ACCESS_TOKEN}" ]
+then
+  fatal "LFA_GITHUB_ACCESS_TOKEN is not defined"
+fi
+
+#------------------------------------------------------------------------
+# Clone credentials repos
+#
+
+info "cloning credentials"
+
+git clone \
+  --depth 1 \
+  "https://${LFA_GITHUB_ACCESS_TOKEN}@github.com/AULFA/credentials" \
+  ".ci/credentials" || fatal "could not clone credentials"
+
+#------------------------------------------------------------------------
+# Import the PGP key for signing Central releases, and try to sign a test
+# file to check that the key hasn't expired.
+#
+
+info "importing GPG key"
+gpg --import ".ci/credentials/android/lfa.asc" || fatal "could not import GPG key"
+
+info "signing test file"
+echo "Test" > hello.txt || fatal "could not create test file"
+gpg --sign -a hello.txt || fatal "could not produce test signature"
+
+#------------------------------------------------------------------------
+# Download Brooklime if necessary.
+#
+
+BROOKLIME_URL="https://repo1.maven.org/maven2/com/io7m/brooklime/com.io7m.brooklime.cmdline/0.1.1/com.io7m.brooklime.cmdline-0.1.1-main.jar"
+BROOKLIME_SHA256_EXPECTED="efce745f90741aa18c9751c5707580abc123088a8de6d6081fff58f8533f9d8e"
+
+wget -O "brooklime.jar.tmp" "${BROOKLIME_URL}" || fatal "could not download brooklime"
+mv "brooklime.jar.tmp" "brooklime.jar" || fatal "could not rename brooklime"
+
+BROOKLIME_SHA256_RECEIVED=$(openssl sha256 "brooklime.jar" | awk '{print $NF}') || fatal "could not checksum brooklime.jar"
+
+if [ "${BROOKLIME_SHA256_EXPECTED}" != "${BROOKLIME_SHA256_RECEIVED}" ]
+then
+  fatal "brooklime.jar checksum does not match.
+  Expected: ${BROOKLIME_SHA256_EXPECTED}
+  Received: ${BROOKLIME_SHA256_RECEIVED}"
+fi
+
