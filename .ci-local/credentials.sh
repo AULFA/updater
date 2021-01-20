4964079ef005905f2e540a078c069e92c0f7de30
Mark Raynsford <code@io7m.com>
2021-01-20 11:20:09 +0000

    Update CI and build configuration

---
+++ .ci-local/credentials.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------
+# A script to copy the necessary credentials into the right places in the
+# build.
+#
+
+#------------------------------------------------------------------------
+# Utility methods
+#
+
+fatal()
+{
+  echo "credentials.sh: fatal: $1" 1>&2
+  exit 1
+}
+
+info()
+{
+  echo "credentials.sh: info: $1" 1>&2
+}
+
+copy()
+{
+  FROM="$1"
+  TO="$2"
+  cp -v "${FROM}" "${TO}" || fatal "could not copy ${FROM} -> ${TO}"
+}
+
+copy .ci/credentials/lfa-keystore.jks lfa-keystore.jks
+
+mkdir -p "$HOME/.gradle" ||
+  fatal "could not create gradle configuration directory"
+cat .ci/credentials/lfa-keystore.properties >> "$HOME/.gradle/gradle.properties" ||
+  fatal "could not write credentials"
