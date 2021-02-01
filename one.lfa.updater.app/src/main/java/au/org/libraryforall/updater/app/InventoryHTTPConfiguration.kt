c58425f9b332cec2742343c685c4616683da68c3
Mark Raynsford <code@io7m.com>
2021-02-01 12:23:26 +0000

    Reduce the HTTP retry count to 5

--- one.lfa.updater.app/src/main/java/au/org/libraryforall/updater/app/InventoryHTTPConfiguration.kt
+++ one.lfa.updater.app/src/main/java/au/org/libraryforall/updater/app/InventoryHTTPConfiguration.kt
@@ -5,7 +5,7 @@ import one.lfa.updater.inventory.api.InventoryHTTPConfigurationType
 object InventoryHTTPConfiguration : InventoryHTTPConfigurationType {
 
   override val retryCount: Int
-    get() = 100
+    get() = 5
 
   override val retryDelaySeconds: Long
     get() = 5L
